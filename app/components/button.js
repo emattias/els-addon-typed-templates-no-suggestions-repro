e3a1623e94199c4b0549a78c7ee34e008591445c
emattias <mattiasha@gmail.com>
2020-05-20 10:51:14 +0200

    repro no autocomplete

---
+++ app/components/button.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+/**
+ * @typedef {Object} Args
+ * @property {boolean} activeTab;
+ */
+export default class Button extends Component {
+  /**
+   * @type {Args}
+   */
+  args;
+}
